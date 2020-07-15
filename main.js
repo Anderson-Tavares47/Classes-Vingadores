e31be8f7ae08750c7d74d4bb59271704de408de5
Anderson Tavares <anderson_felipetavares@hotmail.com>
2020-07-14 23:05:26 -0300

    arrow_functions

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const arr = [1, 2, 3, 4, 5, 6];
-const cesta = ["banana", "maça", "abacate", "abacaxi"];
+const cesta = ["banana", "maçã", "abacate", "abacaxi"];
 
-const squareArr = arr.map(function (item) {
-  return item * item;
-});
-
+const squareArr = arr.map((item) => item * item);
 console.log(squareArr);
 
-const sum = arr.reduce(function (total, next) {
-  return total + next;
-});
+const fruitPosition = cesta.map(
+  (item, index) => `A/O ${item} está na posição ${index} do array`
+);
+
+console.log(fruitPosition);
+
+const sum = arr.reduce((total, next) => total + next);
 
 console.log(sum);
+
+const filter = arr.filter((item) => item % 2 == 0);
+
+console.log(filter);
+
+const find = arr.find((item) => item === 4);
+
+console.log(find);
